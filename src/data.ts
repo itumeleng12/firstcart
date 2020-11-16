ed4b8c8e2708bcb9a70261c6c63e5215a8d562cd
itumeleng12 <itulollie@gmail.com>
2020-11-16 10:59:57 +0200

    first cart

---
+++ src/data.ts
@@ -0,0 +1,45 @@
+import { CartItem } from './app/shopping-cart.service';
+export const DATA_ITEMS: CartItem[] = [
+  {
+    price: 1699.99,
+    image: null,
+    name: '14 INCH',
+    details: 'Peruvian, Brazilian, Magnolian',
+    heart: null,
+  },
+  {
+    price: 1999.99,
+    image: null,
+    name: '16 INCH',
+    details: 'Peruvian, Brazilian, Magnolian',
+    heart: null,
+  },
+  {
+    price: 2199.99,
+    image: null,
+    name: '18 INCH',
+    details: 'Peruvian, Brazilian, Magnolian',
+    heart: null,
+  },
+  {
+    price: 2399.99,
+    image: null,
+    name: '20 INCH',
+    details: 'Peruvian, Brazilian, Magnolian',
+    heart: null,
+  },
+  {
+    price: 2699.99,
+    image: null,
+    name: '22 INCH',
+    details: 'Peruvian, Brazilian, Magnolian',
+    heart: null,
+  },
+  {
+    price: 2999.99,
+    image: null,
+    name: '24 INCH',
+    details: 'Peruvian, Brazilian, Magnolian',
+    heart: null,
+  }
+];
